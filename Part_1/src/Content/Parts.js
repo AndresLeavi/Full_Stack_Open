9ca56e2e89e85828c19034eb25504905fcc30400
Andresleavi <andresleavi092@gmail.com>
2024-10-04 12:32:39 -0300

    Excercise1.1

---
+++ Part_1/src/Content/Parts.js
@@ -0,0 +1,28 @@
+
+export const Part1= (props) => {
+
+    return(
+        <div>
+            <p>{props.name}</p>
+           
+        </div>
+    )
+}
+export const Part2= (props) => {
+
+    return(
+        <div>
+            <p>{props.name}</p>
+           
+        </div>
+    )
+}
+export const Part3= (props) => {
+
+    return(
+        <div>
+            <p>{props.name}</p>
+           
+        </div>
+    )
+}
